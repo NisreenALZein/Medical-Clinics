6e52d06e6fa94d3629e8007ccc64270d0d142e91
NisreenALZein <nisreenalzein205@gmail.com>
2025-09-21 21:07:32 +0300

    uuid

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str ;
+
 
 class Appointment extends Model
 {
     use HasFactory;
+
+    protected static function boot(){
+        parent::boot() ;
+        static::creating(function($model){
+            $model->uuid = Str::uuid() ;
+        });
+        static::updating(function($model){
+            if(empty($model->uuid)){
+                $model->uuid = Str::uuid() ;
+            }
+        });
+    } 
 }
